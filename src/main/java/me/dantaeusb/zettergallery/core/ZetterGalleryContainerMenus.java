90b1a320c36c565576a863774f45b4d5b28d0642
Dmitry Burlakov <hi@dantaeusb.me>
2023-03-30 07:06:31 +0300

    #23 Address incorrect space for merchant screen registration

--- src/main/java/me/dantaeusb/zettergallery/core/ZetterGalleryContainerMenus.java
+++ src/main/java/me/dantaeusb/zettergallery/core/ZetterGalleryContainerMenus.java
@@ -1,6 +1,7 @@
 package me.dantaeusb.zettergallery.core;
 
 import me.dantaeusb.zetter.Zetter;
+import me.dantaeusb.zettergallery.ZetterGallery;
 import me.dantaeusb.zettergallery.menu.PaintingMerchantMenu;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraftforge.common.extensions.IForgeContainerType;
@@ -10,7 +11,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ZetterGalleryContainerMenus {
-    private static final DeferredRegister<ContainerType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.CONTAINERS, Zetter.MOD_ID);
+    private static final DeferredRegister<ContainerType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.CONTAINERS, ZetterGallery.MOD_ID);
 
     public static RegistryObject<ContainerType<PaintingMerchantMenu>> PAINTING_MERCHANT = CONTAINERS.register("painting_merchant_container", () -> IForgeContainerType.create(PaintingMerchantMenu::createMenuClientSide));
 
