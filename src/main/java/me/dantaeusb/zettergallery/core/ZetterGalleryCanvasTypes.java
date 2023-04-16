d080d59a0e2f27aef7adaf76066afaa9efc84dce
Dmitry Burlakov <hi@dantaeusb.me>
2023-04-16 21:26:26 +0300

    Add class to data type to allow casting magic (probably not needed but anyways)

--- src/main/java/me/dantaeusb/zettergallery/core/ZetterGalleryCanvasTypes.java
+++ src/main/java/me/dantaeusb/zettergallery/core/ZetterGalleryCanvasTypes.java
@@ -12,9 +12,10 @@ import net.minecraftforge.registries.RegistryObject;
 public class ZetterGalleryCanvasTypes {
     private static final DeferredRegister<CanvasDataType<?>> CANVAS_TYPES = DeferredRegister.create(ZetterRegistries.CANVAS_TYPE_REGISTRY_NAME, ZetterGallery.MOD_ID);
 
-    public static final RegistryObject<CanvasDataType<GalleryPaintingData>> GALLERY_PAINTING = CANVAS_TYPES.register(GalleryPaintingData.TYPE, () -> new CanvasDataType<>(
+    public static final RegistryObject<CanvasDataType<GalleryPaintingData>> GALLERY_PAINTING = CANVAS_TYPES.register(GalleryPaintingData.TYPE, () -> new CanvasDataType<GalleryPaintingData>(
         new ResourceLocation(ZetterGallery.MOD_ID, GalleryPaintingData.TYPE),
-        GalleryPaintingData.BUILDER
+        GalleryPaintingData.BUILDER,
+        GalleryPaintingData.class
     ));
 
     public static void init(IEventBus bus) {
