fee0139a4f193a83e194faf56b662548cd6c0b45
Dmitry Burlakov <hi@dantaeusb.me>
2023-05-30 18:35:45 +0300

    Fix incorrect name for overlay registration

--- src/main/java/me/dantaeusb/zettergallery/core/ZetterGalleryOverlays.java
+++ src/main/java/me/dantaeusb/zettergallery/core/ZetterGalleryOverlays.java
@@ -1,9 +1,9 @@
 package me.dantaeusb.zettergallery.core;
 
 import me.dantaeusb.zetter.core.ZetterOverlays;
-import me.dantaeusb.zetter.storage.PaintingData;
 import me.dantaeusb.zettergallery.ZetterGallery;
 import me.dantaeusb.zettergallery.client.gui.overlay.GalleryPaintingInfoOverlay;
+import me.dantaeusb.zettergallery.storage.GalleryPaintingData;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
@@ -15,7 +15,7 @@ public class ZetterGalleryOverlays {
     @SubscribeEvent
     public static void onRegisterOverlays(RegisterGuiOverlaysEvent event) {
         GalleryPaintingInfoOverlay overlay = new GalleryPaintingInfoOverlay();
-        ZetterOverlays.OVERLAYS.put(PaintingData.OVERLAY_KEY, overlay);
+        ZetterOverlays.OVERLAYS.put(GalleryPaintingData.OVERLAY_KEY, overlay);
         event.registerAbove(VanillaGuiOverlay.HOTBAR.id(), overlay.getId(), overlay);
     }
 }
